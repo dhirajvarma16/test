735a8337fdb407867ef8f578da0425ab1e436d01
Dhiraj varma <49423650+dhirajvarma16@users.noreply.github.com>
2019-08-18 14:39:06 +0530

    Update InvalidCitySearch.java

--- Prudential_test/src/main/java/cucumber_Prudential_test/Prudential_test/InvalidCitySearch.java
+++ Prudential_test/src/main/java/cucumber_Prudential_test/Prudential_test/InvalidCitySearch.java
@@ -19,9 +19,9 @@ public class InvalidCitySearch {
 	
 	@Given("^User navigates to openweathermap website$")
 	public void user_navigates_to_openweathermap_website() throws Throwable{
-		System.setProperty("webdriver.chrome.driver","/Users/naveenkhunteta/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver","F:\\stuff\\Chromedriver.exe");
 		 driver = new ChromeDriver();
-		 driver.get("https://www.freecrm.com/index.html");
+		 driver.get("https://openweathermap.org/");
 
 	}
 	
@@ -29,7 +29,7 @@ public class InvalidCitySearch {
 	public void user_pageTitle() throws Throwable{
 		String title = driver.getTitle();
 		 System.out.println(title);
-		 Assert.assertEquals("#1 Free CRM for Any Business: Online Customer Relationship Software", title);
+		 Assert.assertEquals("Сurrent weather and forecast - OpenWeatherMap", title);
 	}
 	@Then("^User enters invalid city name$")
 	public void Invalid_search() {
